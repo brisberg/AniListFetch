ce5dd7e8f86af6556025cc4bd88f1b506bcd0584
JeffRisberg <jrisberg@aol.com>
2016-02-14 18:41:48 -0800

    Initial commit

---
+++ src/main/java/com/incra/HelloService.java
@@ -0,0 +1,10 @@
+package com.incra;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloService {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
